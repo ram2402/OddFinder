88a0e9875deae8fea98f6b8cc14e69129442482e
ramesh.thippana <ramesh.thippana@aa.com>
2018-07-23 22:31:14 -0500

    Solution with streams

--- src/main/java/FindOdd.java
+++ src/main/java/FindOdd.java
@@ -1,16 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FindOdd {
     public static int findIt(int[] numbers) {
-        List<Integer> numberList = new ArrayList<>();
-        for (int number : numbers) {
-            if(numberList.contains(number)) {
-                numberList.remove(numberList.indexOf(number));
-            } else {
-                numberList.add(number);
+
+        Map<Integer, Long> occurances = Arrays.stream(numbers)
+                .boxed()
+                .collect(Collectors.groupingBy(Integer::intValue,
+                        Collectors.counting()));
+
+        Set<Map.Entry<Integer, Long>> entries = occurances.entrySet();
+        for (Map.Entry<Integer, Long> entry : entries) {
+            if (entry.getValue() % 2 == 1) {
+                return entry.getKey();
             }
         }
-        return numberList.get(0);
+
+        return 0;
     }
 }
